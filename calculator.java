fb8208793f7254b2ddb74ba07b0e6c8392ede592
Shashank C <01fe22bca200@kletech.ac.in>
2024-02-08 10:42:16 +0530

    Code related to basic Arithmatic Operation

---
+++ calculator.java
@@ -0,0 +1,49 @@
+/**
+ * The Calculator class performs basic arithmetic operations such as addition,
+ * subtraction,
+ * multiplication, and division on two operands.
+ */
+class Calculator {
+    float addition(float opreand_1, float operand_2) {
+        return opreand_1 + operand_2;
+    }
+
+    /**
+     * The function performs subtraction on two float operands and returns the
+     * result.
+     * 
+     * @param opreand_1 The first operand for the subtraction operation.
+     * @param operand_2 The second operand in the subtraction operation.
+     * @return the result of subtracting operand_2 from operand_1.
+     */
+
+    float substraction(float opreand_1, float operand_2) {
+        return opreand_1 - operand_2;
+    }
+
+    float multipliacation(float opreand_1, float operand_2) {
+        return opreand_1 * operand_2;
+    }
+
+    float division(float opreand_1, float operand_2) {
+        return opreand_1 / operand_2;
+    }
+
+    public static void main(String[] args) {
+        float opreand_1 = 10;
+        float operand_2 = 5;
+        Calculator calc = new Calculator();
+
+        float add_result = calc.addition(opreand_1, operand_2);
+        System.out.println("Addition result: " + add_result);
+
+        float sub_result = calc.substraction(opreand_1, operand_2);
+        System.out.println("Subtraction result: " + sub_result);
+
+        float mul_result = calc.multipliacation(opreand_1, operand_2);
+        System.out.println("Multiplication result: " + mul_result);
+
+        float div_result = calc.division(opreand_1, operand_2);
+        System.out.println("Division result: " + div_result);
+    }
+}
