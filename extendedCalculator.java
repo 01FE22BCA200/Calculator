f148fa93115bbc1170f43ce1f8b4b2c2dd18296e
darshank455 <darshankolvekar455@gmail.com>
2024-02-14 14:20:39 +0530

    calculator gitbranching

---
+++ extendedCalculator.java
@@ -0,0 +1,17 @@
+public class extendedCalculator{
+    int square(int operand1){
+        return operand1 * operand1;
+    }
+
+    int cube (int operand1) {
+        return operand1 * operand1 * operand1;
+    }
+
+    int modulus(int  operand1, int operand2) {
+        return  operand1 % operand2;
+    }
+
+    float area(float base, float height){
+        return  (base*height)/2;
+    }
+}
