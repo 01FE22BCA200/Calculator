e5e732f5a49cbb971d58470ff86950a83f289755
01FE22BCA200 <156279935+01FE22BCA200@users.noreply.github.com>
2024-02-16 14:07:28 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,8 +1,4 @@
-/**
- * The Calculator class performs basic arithmetic operations such as addition,
- * subtraction,
- * multiplication, and division on two operands.
- */
+
 class Calculator {
     float addition(float opreand_1, float operand_2) {
         return opreand_1 + operand_2;
@@ -16,14 +12,9 @@ class Calculator {
         return opreand_1 * operand_2;
     }
 
-    float division(float opreand_1, float operand_2) {
-        return opreand_1 / operand_2;
-    }
+   
 
-    float square(float opreand_1) {
-        return opreand_1 * opreand_1;
-    }
-
+   
     float cube(float opreand_1) {
         return opreand_1 * opreand_1 * opreand_1;
     }
@@ -33,9 +24,7 @@ class Calculator {
         float operand_2 = 5;
         Calculator calc = new Calculator();
 
-        float add_result = calc.addition(opreand_1, operand_2);
-        System.out.println("Addition result: " + add_result);
-
+      
         float sub_result = calc.substraction(opreand_1, operand_2);
         System.out.println("Subtraction result: " + sub_result);
 
@@ -45,9 +34,7 @@ class Calculator {
         float div_result = calc.division(opreand_1, operand_2);
         System.out.println("Division result: " + div_result);
 
-        float sqr_result = calc.square(opreand_1);
-        System.out.println("Square of number: " + sqr_result);
-
+        
         float cbrt_result = calc.cube(opreand_1);
         System.out.println("Cubic root of a number: " + cbrt_result);
 
